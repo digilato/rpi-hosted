73764da2819043220c25b575213e50e7dc092b8f
Christian Gravelle <2364912+cgravelle@users.noreply.github.com>
2022-02-04 17:02:07 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -8,7 +8,7 @@ function error {
 function check_internet() {
   printf "Checking if you are online..."
   
-  if [ wget -q --spider http://github.com ]; then
+  if  wget -q --spider http://github.com; then
     echo "Online. Continuing."
   else
     error "Offline. Go connect to the internet then run the script again."
