781f5f08186b182ea682418a36137516facfdee3
Christian Gravelle <2364912+cgravelle@users.noreply.github.com>
2022-02-04 16:54:12 -0500

    shellcheck

---
+++ docker-compose/compose_pihole.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# could also use (works debian and RPi OS):  $(ip route get 8.8.8.8 | awk '{gsub(".*src",""); print $1; exit}')
+# could also use (works debian and RPi OS): ip=`ip addr show |grep "inet " |grep -v 127.0.0. |head -1|cut -d" " -f6|cut -d/ -f1`
+# Also works Could we use this for Teleport IP Address?: `hostname --all-ip-addresses | awk '{print $1}'` 
+# Need to test on other OS's to see how they work.
+# DOES NOT WORK ON DEBIAN (no eth0!): $(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+
+export EXTERNAL_IP=$(ip route get 8.8.8.8 | awk '{gsub(".*src",""); print $1; exit}')
+
+
+exec docker-compose -f pihole.yml up -d
