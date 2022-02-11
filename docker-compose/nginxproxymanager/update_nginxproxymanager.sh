648e00f336617ddbc0adfede9310cea78f8b3c9e
Christian Gravelle <2364912+cgravelle@users.noreply.github.com>
2022-02-10 19:37:02 -0500

    Update update_nginxproxymanager.sh

--- docker-compose/nginxproxymanager/update_nginxproxymanager.sh
+++ docker-compose/nginxproxymanager/update_nginxproxymanager.sh
@@ -23,4 +23,9 @@ nginx_name=`docker ps | grep nginx-proxy-manager | awk '{print $2}'`
 sudo docker stop $nginx_pid || error "Failed to stop portainer!"
 sudo docker rm $nginx_pid || error "Failed to remove portainer container!"
 sudo docker rmi $nginx_name || error "Failed to remove/untag images from the container!"
+
+echo "Taking the opportunity to backup data volume while stopped (i.e., inactive database)."
+mkdir -p backup
+sudo tar cvfz backup/authelia_"$(date +%Y%m%d)".tar.gz /docker_bind/authelia || error "Failed to back up authelia docker_bind!"
+
 exec docker compose -f nginxproxymanager.yaml up -d || error "Failed to execute newer version of Nginx Proxy Manager!"
