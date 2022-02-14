75a9b628b4976acacd434d2f281bf84550271158
Christian Gravelle <2364912+cgravelle@users.noreply.github.com>
2022-02-14 17:51:45 -0500

    Added CLI tools

---
+++ tools/ctop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+function check_internet() {
+  printf "Checking if you are online..."
+  
+  if wget -q --spider http://github.com; then
+    echo "Online. Continuing."
+  else
+    error "Offline. Go connect to the internet then run the script again."
+  fi
+}
+
+check_internet
+
+docker run --rm -ti \
+  --name=ctop \
+  --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+  quay.io/vektorlab/ctop:latest
