67db396bc89ef0ba5c2ee67ee7e4d419c56624b8
Christian Gravelle <2364912+digilato@users.noreply.github.com>
2022-02-14 23:10:37 -0500

    Update ctop.sh

--- tools/ctop.sh
+++ tools/ctop.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+export VERSION=0.7.6 # Could automate for latest, but individuals might not want this. Find latest at https://goteleport.com/teleport/download/
+opsys=$(uname -s | awk '{print tolower($0)}')  # 'darwin' 'linux' or 'windows'
+export OPSYS
+# TODO: Automate this portion.
+export ARCH=arm # '386' 'arm' on linux or 'amd64' for all distros 
+
+CTOP_PACKAGE=ctop-$VERSION-$OPSYS-$ARCH
+
+
 function error {
   echo -e "\\e[91m$1\\e[39m"
   exit 1
@@ -17,7 +26,11 @@ function check_internet() {
 
 check_internet
 
-docker run --rm -ti \
-  --name=ctop \
-  --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-  quay.io/vektorlab/ctop:latest
+sudo wget "https://github.com/bcicen/ctop/releases/download/$VERSION/$CTOP_PACKAGE" -O /usr/local/bin/ctop
+sudo chmod +x /usr/local/bin/ctop
+
+
+# docker run --rm -ti \
+#  --name=ctop \
+#  --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+#  quay.io/vektorlab/ctop:latest
