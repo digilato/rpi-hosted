8511b19de1b25f7b41e93fd1a4eb92ca9ce80cf4
Christian Gravelle <2364912+digilato@users.noreply.github.com>
2022-03-11 16:30:58 -0500

    Update install_docker_compose.sh

--- docker-compose/install_docker_compose.sh
+++ docker-compose/install_docker_compose.sh
@@ -1,40 +1,61 @@
 #!/bin/bash
 
-export version=v2.2.3 # Could automate for latest, but individuals might not want this. Find latest at https://github.com/docker/compose/releases
-opsys=$(uname -s | awk '{print tolower($0)}')  # 'darwin' 'linux' or 'windows'
-export opsys
-
-# TODO: Automate this portion with uname -m. THe issue is armv7l, armhf, etc., needs to be parsed; case?
-export arch=armv7 # options include 'armv7' 'aarch64' 'armv6' 'x86_64' 
-
-compose_package=docker-compose-$opsys-$arch
-
 
 function error {
   echo -e "\\e[91m$1\\e[39m"
   exit 1
 }
 
+# Make sure we have an internet connection. Can't do anything otherwise.
 function check_internet() {
   printf "Checking if you are online..."
   
-  if  wget -q --spider http://github.com; then
+  if  curl -q --fail http://github.com; then
     echo "Online. Continuing."
   else
     error "Offline. Go connect to the internet then run the script again."
   fi
 }
 
+# Find architecture. Current options include 'armv7' 'aarch64' 'armv6' 'x86_64' 
+function uname_arch() {
+  ARCH=$(uname -m)
+  case $ARCH in
+    x86_64) ARCH="x86_64" ;;
+    aarch64) ARCH="aarch64" ;;
+    armv6*) ARCH="armv6" ;;
+    armv7*) ARCH="armv7" ;;
+    armhf) ARCH="armv7" ;;
+  esac
+  # echo ${ARCH}
+}
+
+#Prepare the file to download
+# Check internet
 check_internet
 
+# Check architecture
+uname_arch
+
+# Get operating system
+OPSYS=$(uname -s | awk '{print tolower($0)}')  # options include 'darwin' 'linux' or 'windows'
+
+# Which package to download
+COMPOSE_PACKAGE=docker-compose-$OPSYS-$ARCH
+
+# Get latest version
+GITHUB_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/docker/compose/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+
+# Prepare the download URL
+GITHUB_URL="https://github.com/docker/compose/releases/download/$GITHUB_VERSION/$COMPOSE_PACKAGE"
+
+
+
+
 sudo mkdir -p /usr/local/lib/docker/cli-plugins || error "Failed to create plugins directory."
-echo "Downloading Docker Compose $version from https://github.com/docker/compose/releases/download/$version/$compose_package"
-sudo curl -SL "https://github.com/docker/compose/releases/download/$version/$compose_package" -o /usr/local/lib/docker/cli-plugins/docker-compose || error "Failed to download Docker Compose."
+echo "Downloading Docker Compose $GITHUB_VERSION for \"$OPSYS $ARCH\" from $GITHUB_URL"
+sudo curl -SL "$GITHUB_URL" -o /usr/local/lib/docker/cli-plugins/docker-compose || error "Failed to download Docker Compose."
 sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose || error "Failed to add executable."
 printf "Installed: "
 exec docker compose version
 
-# Install older version
-# curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose || error "Failed to install Docker Compose."
-
-# 
