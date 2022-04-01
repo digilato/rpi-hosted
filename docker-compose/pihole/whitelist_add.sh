05f9b70e8a5d9f722b5dc0602966298f57ce4eb2
Christian Gravelle <2364912+digilato@users.noreply.github.com>
2022-04-01 16:40:58 -0400

    Create whitelist_add.sh

---
+++ docker-compose/pihole/whitelist_add.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+command -v python3 >/dev/null 2>&1 && echo Python 3 is installed  # POSIX-compliant
+
+
+
+type -P python3 >/dev/null 2>&1 && echo Python 3 is installed     # Bash only
